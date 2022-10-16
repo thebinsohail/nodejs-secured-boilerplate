27bb6a8f5a32e7c9784a05bf92058e967671038c
thebinsohail <anasbinsohail@outlook.com>
2022-10-17 03:15:03 +0500

    admin can delete users

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -2,12 +2,14 @@ const express = require('express')
 const dotenv = require('dotenv');
 const router = express.Router();
 const middlewares = require("../middlewares/middlewares");
+const { User } = require("../models");
 
 // Set up Global configuration access
 dotenv.config({ path: "../.env" });
 
 // if the user has role "ROLE_ADMIN" it will allow to access else no access!
 router.get("/manage", [middlewares.tokenAuthentication, middlewares.roleAuthentication('ROLE_ADMIN')],
+
     (req, res) => {
         res.json({
             auth: true,
@@ -15,6 +17,31 @@ router.get("/manage", [middlewares.tokenAuthentication, middlewares.roleAuthenti
             body: req.user
         })
     });
+
+// ADMIN CAN DELETE THE USER
+router.delete("/delete/user", [middlewares.tokenAuthentication, middlewares.roleAuthentication('ROLE_ADMIN')],
+    (req, res) => {
+
+        try {
+            User.destroy(
+                { where: { email: req.body.email }, force: true },
+            ).then((recordDeleted) => {
+                if (recordDeleted === 1) {
+                    res.json({ message: 'User deleted successfully!' });
+                }
+                else {
+                    res.status(404).send({ message: 'User not found!' });
+                }
+            });
+
+        } catch (error) {
+            res.status(500).json({ message: error });
+        }
+
+    });
+
+
+
 
 
 module.exports = router;
